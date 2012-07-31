53db20a3424d10a96fee6dd4daf86337433beaa8
Jonas Westerlund <jonas.westerlund@me.com>
2012-07-31 19:30:29 +0200

    Lots of refactoring
    Trying out some JS language extensions.

--- factoid.js
+++ factoid.js
@@ -2,82 +2,78 @@
  * @module factoid
  */
 
-const fmt     = require( "util" ).format
-    , irc     = require( "irc-js" )
-    , shared  = require( "./shared" )
+"use strict";
 
-const log   = irc.logger.get( "ircjs-plugin-factoid" )
-    , sKey  = "FACTOID"
+const fmt     = require("util").format;
+const irc     = require("irc-js");
+const shared  = require("./shared");
+
+const log   = irc.logger.get("ircjs-plugin-factoid");
+const sKey  = "FACTOID";
 
 // Factoids.
 
-const redisClient = shared.redis.client
+const redisClient = shared.redis.client;
 
-const speak = function( client, msg, trigger, person ) {
-  redisClient.hget( sKey, trigger, function( err, res ) {
-    if ( err ) {
-      log.error( "Error hget:ing factoid: %s", err )
+function speak(msg, trigger, person) {
+  redisClient.hget(sKey, trigger, function(err, res) {
+    if (err) {
+      log.error("Error hget:ing factoid: %s", err)
       return
     }
-    if ( ! res )
-      return
-    msg.reply( "%s, %s", person || msg.from.nick, res )
-    return irc.STATUS.STOP
-  } )
-}
-
-const learn = function( msg, key, value ) {
-  log.debug( "factoid learn `%s`", key )
-  redisClient.hset( sKey, key, value, function( err, res ) {
-    if ( err ) {
-      log.error( "learn factoid error: %s", err )
+    if (!res) {
       return
     }
-    log.debug( "Learned a new factoid: %s", key )
-    msg.reply( "%s, memorised “%s”.", msg.from.nick, key )
-  } )
+    msg.reply("%s, %s", person || msg.from.nick, res);
+    return irc.STATUS.STOP;
+  });
 }
 
-const forget = function( msg, key ) {
-  log.debug( "factoid forget `%s`", key )
-  redisClient.hdel( sKey, key, function( err, res ) {
-    if ( err ) {
-      log.error( "forget factoid error: %s", err )
-      return
+function learn(msg, key, value) {
+  log.debug("factoid learn `%s`", key);
+  redisClient.hset(sKey, key, value, function(err, res) {
+    if (err) {
+      log.error("learn factoid error: %s", err);
+      return;
+    }
+    log.debug("Learned a new factoid: %s", key);
+    msg.reply("%s, memorised “%s”.", msg.from.nick, key);
+  });
+}
+
+function forget(msg, key) {
+  log.debug("factoid forget `%s`", key);
+  redisClient.hdel(sKey, key, function(err, res) {
+    if (err) {
+      log.error("forget factoid error: %s", err);
+      return;
     }
     // Nothing was deleted
-    if ( res === 0 ) {
-      msg.reply( "%s, I can’t forget that which I do not know.", msg.from.nick )
-      return
+    if (res === 0) {
+      msg.reply("%s, I can’t forget that which I do not know.", msg.from.nick);
+      return;
     }
-    const replyText = fmt( "%s, I have forgotten “%s”%s"
-      , msg.from.nick, key
-      , Math.random() > 0.5 ? ". My mind is going, I can feel it." : "." )
-    msg.reply( replyText )
-    log.debug( "Happily forgot factoid: %s", key )
-  } )
+    const replyText = fmt("%s, I have forgotten “%s”%s", msg.from.nick, key,
+      Math.random() > 0.5 ? ". My mind is going, I can feel it." : ".");
+    msg.reply(replyText);
+    log.debug("Happily forgot factoid: %s", key);
+  });
 }
 
 // Implement Plugin interface.
 
-const load = function( client ) {
-  const signal =
-    client.listen( irc.COMMAND.PRIVMSG )
-          .filter( shared.filter.forMe.bind( null, client ) )
-          .filter( shared.filter.notForBotT.bind( null, client ) )
-  signal.match( /([-_.:|\/\\\w]+) +is[:,]? +(.+)$/ )
-        .receive( learn )
-  signal.match( /forget\s+([-_.:|\/\\\w]+)$/ )
-        .receive( forget )
-  signal.match( /([-_.:|\/\\\w]+)(?:\s*@\s*([-\[\]\{\}`|_\w]+))?\s*$/ )
-        .receive( speak.bind( null, client ) )
-  return irc.STATUS.SUCCESS
+function load(bot) {
+  bot.matchIf(/^:(?:\W|\S+\W?)?(\w\S+) +is[:,]? +(.+)/, shared.forMe, learn);
+  bot.matchIf(/\bforget\s+([-_.:|\/\\\w]+)$/, shared.forMe, forget);
+  bot.matchIf(/([-_.:|\/\\\w]+)(?:\s*@\s*([-\[\]\{\}`|_\w]+))?\s*$/,
+    shared.forMe, speak);
+  return irc.STATUS.SUCCESS;
 }
 
-const unload = function() {
-  return irc.STATUS.SUCCESS
+function unload() {
+  return irc.STATUS.SUCCESS;
 }
 
-exports.name    = "Factoid"
-exports.load    = load
-exports.unload  = unload
+exports.name    = "Factoid";
+exports.load    = load;
+exports.unload  = unload;
--- jqapi.js
+++ jqapi.js
@@ -1,66 +1,71 @@
-const fmt     = require( "util" ).format
-    , https   = require( "https" )
-    , irc     = require( "irc-js" )
-    , shared  = require( "./shared" )
-    , jQJSON  = require( "./jqapi.json" )
-    , log     = irc.logger.get( "ircjs-plugin-jqapi" )
+/** @module jqapi */
 
-const onAPI = function( msg, query, index, nick ) {
-  log.debug( "in onAPI" )
-  const results = []
-      , num     = index ? index : 1
-      , replyTo = nick || msg.from.nick
-  var hits = 0
-    , name
-    , title
-    , desc
-    , url
-    , key
-  log.debug( "interate" )
-  for ( key in jQJSON ) {
-    name  = jQJSON[ key ].name
-    title = jQJSON[ key ].title
-    desc  = jQJSON[ key ].desc
-    url   = jQJSON[ key ].url
+"use strict";
+
+const fmt     = require("util").format;
+const https   = require("https");
+const irc     = require("irc-js");
+const shared  = require("./shared");
+const jQJSON  = require("./jqapi.json");
+const log     = irc.logger.get("ircjs-plugin-jqapi");
+
+function onAPI(msg, query, index, nick) {
+  log.debug("in onAPI")
+  const results = [];
+  const num     = index ? index : 1;
+  const replyTo = nick || msg.from.nick;
+  let hits = 0;
+  let name;
+  let title;
+  let desc;
+  let url;
+  let key;
+  for (key in jQJSON) {
+    name  = jQJSON[key].name;
+    title = jQJSON[key].title;
+    desc  = jQJSON[key].desc;
+    url   = jQJSON[key].url;
     // search for selector
-    if ( -1 !== query.indexOf( ":" )
-        && title.indexOf( "Selector" ) >= 0
-        && name === query.replace( ":", "" ) ) {
-      results.push( fmt( "%s: %s %s", title, desc, url ) )
-      ++hits
+    if (-1 !== query.indexOf(":") &&
+        title.indexOf("Selector") >= 0 &&
+        name === query.replace(":", "")) {
+      results.push(fmt("%s: %s %s", title, desc, url));
+      ++hits;
     }
     // everything else
-    else if ( name.toLowerCase() === query.toLowerCase()
-        && title.indexOf( ":" ) === -1 ) {
-      results.push( fmt( "%s: %s %s", title, desc, url ) )
-      ++hits
+    else if (name.toLowerCase() === query.toLowerCase() &&
+        title.indexOf(":") === -1) {
+      results.push(fmt("%s: %s %s", title, desc, url));
+      ++hits;
     }
-    if ( hits === num )
-      break
+    if (hits === num) {
+      break;
+    }
   }
-  log.debug( "checkcizncn", hits )
-  if ( hits )
-    if ( index && hits < index )
-      msg.reply( "%s, I only found %d match%s.", replyTo, hits, hits === 1 ? "" : "es" )
-    else
-      msg.reply( "%s, %s", replyTo, results.pop() )
-  else
-    msg.reply( "%s, no matches.", replyTo )
-  return irc.STATUS.STOP
+  if (hits) {
+    if (index && hits < index) {
+      msg.reply("%s, I only found %d match%s.", replyTo, hits, hits === 1 ? "" : "es");
+    }
+    else {
+      msg.reply("%s, %s", replyTo, results.pop());
+    }
+  }
+  else {
+    msg.reply("%s, no matches.", replyTo);
+  }
+  return irc.STATUS.STOP;
 }
 
-const load = function( client ) {
-  client.listen( irc.COMMAND.PRIVMSG )
-        .filter( shared.filter.forMe.bind( null, client ) )
-        .match( /\bapi\s+(\S+)(?:\s*#([1-9]))?(?:\s*@\s*(\S+)[\s\W]*)?$/ )
-        .receive( onAPI )
-  return irc.STATUS.SUCCESS
+function load(client) {
+  client.matchIf(/\bapi\s+(\S+)(?:\s*([1-9]+))?(?:\s*@\s*(\S+)[\s\W]*)?$/,
+    shared.forMe, onAPI);
+  return irc.STATUS.SUCCESS;
 }
 
-const unload = function() {
-  return irc.STATUS.SUCCESS
+function unload() {
+  return irc.STATUS.SUCCESS;
 }
 
-exports.name    = "jQuery API"
-exports.load    = load
-exports.unload  = unload
+exports.name    = "jQuery API";
+exports.load    = load;
+exports.unload  = unload;

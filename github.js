722c7d5a4a003a1ae25cb5e98d28703765dbe427
Andrée Hansson <peolanha@gmail.com>
2013-01-09 19:19:44 +0100

    Added support for github commits, issues, and pull requests

---
+++ github.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const irc     = require("irc-js");
+const shared  = require("./shared");
+const log     = irc.logger.get("ircjs-plugin-github");
+
+const host    = "api.github.com";
+
+function commit(msg, owner, project, sha) {
+  log.debug("Fetching commit: %s", sha);
+  const url = { hostname: host, path: "/repos/"+owner+"/"+project+"/git/commits/"+sha };
+  shared.getJSON(url, function(data) {
+    let ago = shared.timeAgo(new Date(data.author.date));
+    msg.reply("%s, %s (%s): %s", msg.from.nick, data.author.name, ago, data.message);
+  });
+}
+
+function issue(msg, owner, project, num) {
+  log.debug("Fetching issue: %s on %s", num, project);
+  const url = { hostname: host, path: "/repos/"+owner+"/"+project+"/issues/"+num };
+  shared.getJSON(url, function(data) {
+    let ago = shared.timeAgo(new Date(data.created_at));
+    msg.reply("%s, issue #%d by %s (%s): %s", msg.from.nick, num, data.user.login, ago, data.title);
+  });
+}
+
+function pullRequest(msg, owner, project, num) {
+  log.debug("Fetching pull requst: %s on %s", num, project);
+  const url = { hostname: host, path: "/repos/"+owner+"/"+project+"/pulls/"+num };
+  shared.getJSON(url, function(data) {
+    let ago = shared.timeAgo(new Date(data.created_at));
+    log.debug("pull data", data);
+    msg.reply("%s, pull request #%d by %s (%s): %s", msg.from.nick, num, data.user.login, ago, data.title);
+  });
+}
+
+function load(bot) {
+  bot.match(/github\.com\/(.+?)\/(.+?)\/commit\/(\S+)/i, commit);
+  bot.match(/github\.com\/(.+?)\/(.+?)\/issues\/(\d+)/i, issue);
+  bot.match(/github\.com\/(.+?)\/(.+?)\/pull\/(\d+)/i, pullRequest);
+  return irc.STATUS.SUCCESS;
+}
+
+function unload() {
+  return irc.STATUS.SUCCESS;
+}
+
+exports.name    = "GitHub";
+exports.load    = load;
+exports.unload  = unload;

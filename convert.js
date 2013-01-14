6390d661e8b57651da2c2c3d2995343e18df56ae
Gianni Chiappetta <gianni@runlevel6.org>
2013-01-14 11:39:16 -0800

    Code style

--- convert.js
+++ convert.js
@@ -9,44 +9,37 @@ const http    = require("http");
 const irc     = require("irc-js");
 const shared  = require("./shared");
 const log     = irc.logger.get("ircjs-plugin-convert");
-const request = require('request'); 
-
 
 function onConvert(msg, query, index, nick) {
-  
-  const q =  query.split(/in|to/)
-    , replyTo = nick || msg.from.nick
-    , url = {
-        host: "www.google.com",
-        path: "/ig/calculator?hl=en&q=" + encodeURI(query)
-      };
+  const q =  query.split(/in|to/);
+  const replyTo = nick || msg.from.nick;
+  const url = {
+    host: "www.google.com",
+    path: "/ig/calculator?hl=en&q=" + encodeURI(query)
+  };
 
   http.get(url, function(res) {
     const data = [];
     res
       .on(irc.NODE.SOCKET.EVENT.DATA, data.push.bind(data))
       .on(irc.NODE.SOCKET.EVENT.END, function() {
-        
-        // const j = JSON.parse(data.join(""));
-        var j = data.join("");
-        j = j.replace(/(['"])?([a-zA-Z0-9]+)(['"])?:/g, '"$2":');
-        j = JSON.parse(j);
+        const j = JSON.parse(
+          data
+            .join("")
+            .replace(/(['"])?([a-zA-Z0-9]+)(['"])?:/g, '"$2":'));
 
-        if(j.error){
+        if (j.error) {
           msg.reply("Error converting...");
-          return;
         }
-
-        var response = j.lhs + " \x02=\x02 " + j.rhs;
-        msg.reply(response);
-
+        else {
+          var response = j.lhs + " \x02=\x02 " + j.rhs;
+          msg.reply(response);
+        }
       });
 
-  return irc.STATUS.STOP;
+    return irc.STATUS.STOP;
   });
-
 }
-
 
 function load(bot) {
   bot.match(/^:[!,./\?@`]calc(?:ulate)?\s+([^#@]+)(?:\s*#(\d+))?(?:\s*@\s*(\S+))?\s*$/i, onConvert);

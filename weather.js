8fbcb9594812b3a576aeeae2f229e1de3d0d6cb0
Gianni Chiappetta <gianni@runlevel6.org>
2013-01-23 01:09:53 -0800

    Weather updates

--- weather.js
+++ weather.js
@@ -2,25 +2,25 @@
  * @module weather
  */
 
-"use strict";
+'use strict';
 
-const fmt     = require("util").format;
-const http    = require("http");
-const irc     = require("irc-js");
-const shared  = require("./shared");
-const log     = irc.logger.get("ircjs-plugin-weather");
+const fmt     = require('util').format;
+const http    = require('http');
+const irc     = require('irc-js');
+const shared  = require('./shared');
+const log     = irc.logger.get('ircjs-plugin-weather');
 const APIKEYS = ['dc2f9ffc1dacb602','63dae8bbfe95a2b8']
 const icons   = {
-  "snow":      "☃❄",
-  "clear":     "☼",
-  "sun":       "☀",
-  "cloud":     "☁",
-  "rain":      "☔",
-  "overcast":  "☁",
-  "shower":    "☂☁",
-  "lightning": "⚡",
-  "thunder":   "⚡",
-  "haze":      "≈"
+  'snow':      '☃❄',
+  'clear':     '☼',
+  'sun':       '☀',
+  'cloud':     '☁',
+  'rain':      '☔',
+  'overcast':  '☁',
+  'shower':    '☂☁',
+  'lightning': '⚡',
+  'thunder':   '⚡',
+  'haze':      '≈'
 }
 
 var crew
@@ -30,18 +30,18 @@ function onWeather(msg, query, index, nick) {
     query = msg.from.nick;
   }
 
-  /* See if we are looking for someone in the crews weather */
+  // See if we are looking for someone in the crews weather
   var member = crew.filter(function(v){ return v.irc.toLowerCase() === query.toLowerCase() });
 
   if (member.length) {
     query = member[0].location;
   }
 
-  const q = query.replace(/ /g, "+");
+  const q = query.replace(/ /g, '+');
   const replyTo = nick || msg.from.nick;
   const url = {
-    host: "api.wunderground.com",
-    path: "/api/" + APIKEYS[Math.floor(Math.random() * APIKEYS.length)] + "/conditions/q/" + encodeURI(query) + ".json"
+    host: 'api.wunderground.com',
+    path: fmt('/api/%s/conditions/q/%s.json', APIKEYS[Math.floor(Math.random() * APIKEYS.length)], encodeURI(query))
   };
 
   http.get(url, function(res) {
@@ -49,42 +49,33 @@ function onWeather(msg, query, index, nick) {
     res
       .on(irc.NODE.SOCKET.EVENT.DATA, data.push.bind(data))
       .on(irc.NODE.SOCKET.EVENT.END, function() {
-        const j = JSON.parse(data.join(""));
+        const j = JSON.parse(data.join(''));
+        var obs;
 
         if (j.response.error) {
           msg.reply(j.response.error.description);
         }
         else if (j.response.results) {
-          msg.reply("Found %s cities searching for %s, please be more specific!", j.response.results.length, query);
+          msg.reply(fmt('found %s cities searching for %s, please be more specific!', j.response.results.length, query));
         }
-        else if (j.current_observation) {
-          var key,
-              currIcons = [],
-              feelslike = "";
+        else if (obs = j.current_observation) {
+          // Feels like
+          const feelsLike = obs.temp_f != obs.feelslike_f ?
+            fmt('Feels like %s. ', format_temp(obs.feelslike_f, obs.feelslike_c)) :
+            '';
 
           // Get icons for current weather
-          for (key in icons) {
-              if (j.current_observation.weather.toLowerCase().match(key, "i")) {
-                  currIcons.push(icons[key]);
-              }
-          }
+          const currIcons = Object.keys(icons)
+            .filter(function(i) { return RegExp(i, 'i').test(obs.weather); })
+            .map(function(i) { return icons[i]; })
+            .join('');
 
-          if (currIcons.length) {
-            currIcons = currIcons.join("");
-          }
-
-          // See if the feels like weather is different
-          if (j.current_observation.temp_f != j.current_observation.feelslike_f) {
-            feelslike = " Feels like \x02" + j.current_observation.feelslike_f + "°F\x02/\x02" + j.current_observation.feelslike_c + "°C\x02";
-          }
-
-          msg.reply("%s %s \x02%s°F\x02/\x02%s°C\x02. %s [%s]",
+          msg.reply(fmt('%s %s %s. %s%s',
                     currIcons,
-                    j.current_observation.weather,
-                    j.current_observation.temp_f,
-                    j.current_observation.temp_c,
-                    feelslike,
-                    j.current_observation.display_location.full);
+                    obs.weather,
+                    format_temp(obs.temp_f, obs.temp_c),
+                    feelsLike,
+                    obs.display_location.full));
         }
       });
   });
@@ -92,18 +83,21 @@ function onWeather(msg, query, index, nick) {
   return irc.STATUS.STOP;
 }
 
+function format_temp (f, c) {
+  return fmt("\x02%s°F\x02/\x02%s°C\x02", f, c)
+}
 
 function load(bot) {
-    // Load the crew
-    shared.getJSON('https://raw.github.com/ot-crew/ot-crew.com/master/public/crew.json', function (obj) {
-      crew = obj;
-    });
+  // Load the crew
+  shared.getJSON('https://raw.github.com/ot-crew/ot-crew.com/master/public/crew.json', function (obj) {
+    crew = obj;
+  });
 
-    bot.match(/^:[!,./\?@`]w(?:eather)?\s+([^#@]+)(?:\s*#(\d+))?(?:\s*@\s*(\S+))?\s*$/i, onWeather);
-    bot.match(/^:[!,./\?@`]w(?:eather)?\s*$/, onWeather);
+  bot.match(/^:[!,./\?@`]w(?:eather)?\s+([^#@]+)(?:\s*#(\d+))?(?:\s*@\s*(\S+))?\s*$/i, onWeather);
+  bot.match(/^:[!,./\?@`]w(?:eather)?\s*$/, onWeather);
 
-    return irc.STATUS.SUCCESS;
-  }
+  return irc.STATUS.SUCCESS;
+}
 
 function unload() {
   return irc.STATUS.SUCCESS;

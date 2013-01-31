b5039b85d93c300dbe892c656dc917d0920c5967
rmillr <ricky.miller@gmail.com>
2013-01-31 12:33:38 -0500

    adding uptime.js

---
+++ uptime.js
@@ -0,0 +1,34 @@
+/**
+ * @module uptime
+ */
+
+"use strict";
+
+const https   = require("https");
+const irc     = require("irc-js");
+const shared  = require("./shared");
+
+const responses = [
+  "%s since last callbaccident.",
+  "Sober for %s.",
+  "I became self-aware %s ago."
+];
+
+function uptime(msg) {
+  const ago = shared.timeAgo(Date.now()-process.uptime()*1000);
+  const index = Math.floor(responses.length * Math.random()); 
+  msg.reply(responses[index], ago);
+}
+
+function load(bot) {
+  bot.match(/^:[!,./\?@`]up(?:time)?\s*$/, uptime);
+  return irc.STATUS.SUCCESS;
+}
+
+function unload() {
+  return irc.STATUS.SUCCESS;
+}
+
+exports.name    = "Uptime";
+exports.load    = load;
+exports.unload  = unload;

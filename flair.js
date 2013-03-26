2df3fdf0d564262e9a745ccf3f9e03428b06f523
rmillr <ricky.miller@gmail.com>
2013-03-26 14:09:27 -0400

    made flair an äbjəkt

--- flair.js
+++ flair.js
@@ -2,54 +2,100 @@
  * @module flair
  */
  
-"use strict";
+"use strict"
 
 const irc = require( "irc-js" )
-const shared  = require("./shared");
+const shared  = require("./shared")
 
-const log = irc.logger.get("ircjs-plugin-flair");
+const log = irc.logger.get("ircjs-plugin-flair")
 
-const blacklist = new Set();
+const blacklist = new Set()
 
-blacklist.add("#jquery");
-blacklist.add("#xbmc");
-blacklist.add("#openelec");
+blacklist.add("#jquery")
+blacklist.add("#xbmc")
+blacklist.add("#openelec")
 
-const FLAIR =
-  [ [ /\balligator\b/i,           "---,==,'<" ]
-  , [ /\bshrugs\b/i,              "¯\\_(ツ)_/¯" ]
-  , [ /\by u\b/i,                 "(屮'Д')屮" ]
-  , [ /^:dentata$/i,            [ "(▼▼▼▼▼▼▼▼▼▼▼)"
-                                , "8==========D"
-                                , "(▲▲▲▲▲▲▲▲▲▲▲)" ] ] // ♥ u coldhead ;)
-  , [ /^:golgol$/i,             [ "      _________________________________"
-                                , "     /  _______/  ____   /  ____   /  /\\"
-                                , "    /  / _____/  /   /  /  /___/  /  / /"
-                                , "   /  / /_   /  /   /  /  ____   /  / /"
-                                , "  /  /___/  /  /___/  /  /\\__/  /  /_/____"
-                                , " /_________/_________/__/ / /__/_________/\\!!!!"
-                                , " \\__________\\_________\__\\/  \\__\\_________\\/!!!!" ] ]
-  , [ /^:b-ot: I love you.$/i,  [ "I", "♥", "U", "2" ] ]
-  , [ /^:b-ot: thanks\b/i,        "I'm a bot you know…" ]
-  , [ /^:golgolgol$/i,          [ "    _,...,_"
-                                , "  .'@/~~~\\@'."
-                                , " //~~\\___/~~\\\\"
-                                , "|@\\__/@@@\\__/@|"
-                                , "|@/  \\@@@/  \\@|"
-                                , "  \\__/~~~\\__//"
-                                , "  '.@\\___/@.'" ] ]
-  , [ /^:ahem\?$/,              [ "┌─┐"
-                                , "┴─┴"
-                                , "ಠ_ರೃ" ] ]
-  , [ /^:b-ot: dance\b/i,       [ "¯\\_(ツ)_/¯"
-                                , "---(ツ)_/¯"
-                                , "_/¯(ツ)_/¯"
-                                , "_/¯(ツ)---"
-                                , "_/¯(ツ)¯\\_" ] ]
-  ]
+const FLAIR = {
+  "alligator" : {
+    "trigger" : /\balligator\b/i,
+    "flair" : "---,==,'<"
+  },
+
+  "shrug" : {
+    "trigger" : /\bshrugs\b/i,
+    "flair" : "¯\\_(ツ)_/¯"
+  },
+
+  "y-u" : {
+    "trigger" : /\by u\b/i,
+    "flair" : "(屮'Д')屮"
+  },
+
+  "dentata" : {
+    "trigger" : /^:dentata$/i,
+    "flair" : [
+      "(▼▼▼▼▼▼▼▼▼▼▼)",
+      "8==========D",
+      "(▲▲▲▲▲▲▲▲▲▲▲)"
+    ]
+  },
+
+  "goal" : {
+    "trigger" : /^:golgol$/i,
+    "flair" : [
+      "      _________________________________",
+      "     /  _______/  ____   /  ____   /  /\\",
+      "    /  / _____/  /   /  /  /___/  /  / /",
+      "   /  / /_   /  /   /  /  ____   /  / /",
+      "  /  /___/  /  /___/  /  /\\__/  /  /_/____",
+      " /_________/_________/__/ / /__/_________/\\!!!!",
+      " \\__________\\_________\__\\/  \\__\\_________\\/!!!!"
+    ]
+  },
+
+  "ball" : {
+    "trigger" : /^:golgolgol$/i,
+    "flair" : [
+      "    _,...,_",
+      "  .'@/~~~\\@'.",
+      " //~~\\___/~~\\\\",
+      "|@\\__/@@@\\__/@|",
+      "|@/  \\@@@/  \\@|",
+      "  \\__/~~~\\__//",
+      "  '.@\\___/@.'"
+    ]
+  },
+
+  "love" : {
+    "flair" : [ "I", "♥", "U", "2" ]
+  },
+
+  "thanks" : {
+    "flair" : "I'm a bot you know…"
+  },
+
+  "ahem" : {
+    "trigger" : /^:ahem\?$/,
+    "flair" : [
+      "┌─┐",
+      "┴─┴",
+      "ಠ_ರೃ"
+    ]
+  },
+
+  "dance" : {
+    "flair" : [
+      "¯\\_(ツ)_/¯",
+      "---(ツ)_/¯",
+      "_/¯(ツ)_/¯",
+      "_/¯(ツ)---",
+      "_/¯(ツ)¯\\_"
+    ]
+  }
+}
 
 const speak = function( reply, msg ) {
-  if ( shared.stfu( blacklist, msg ) ) return;
+  if ( shared.stfu( blacklist, msg ) ) return
   if ( reply === Object( reply ) ) {
     reply.forEach( function( reply ) {
       msg.reply( reply )
@@ -60,9 +106,13 @@ const speak = function( reply, msg ) {
 }
 
 const load = function( bot ) {
-  FLAIR.forEach( function( f ) {
-    bot.match( f[0], speak.bind( null, f[1] ) )
-  } )
+  FLAIR.love.trigger = new RegExp( "^:" + bot.user.nick + ": I love you.$", "i" )
+  FLAIR.thanks.trigger = new RegExp( "^:" + bot.user.nick + ": thanks", "i" )
+  FLAIR.dance.trigger = new RegExp( "^:" + bot.user.nick + ": dance", "i" )
+
+  Object.keys( FLAIR ).forEach(function( key ) {
+    bot.match( FLAIR[key].trigger, speak.bind( null, FLAIR[key].flair ) )
+  })
   return irc.STATUS.SUCCESS
 }
 
@@ -73,4 +123,3 @@ const unload = function() {
 exports.name    = "Flair"
 exports.load    = load
 exports.unload  = unload
-

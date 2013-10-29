b97d9a2181d97f35d7ebe4a50cf92b67a2c6800e
Steve De Jonghe <seutje@gmail.com>
2013-10-29 16:20:55 +0100

    brainfart
    I'm a moron

--- github.js
+++ github.js
@@ -12,8 +12,8 @@ function commit(msg, owner, project, sha) {
   shared.getJSON(url, function(data) {
     let ago = shared.timeAgo(new Date(data.author.date)).slice(0, 2).join(" ");
     msg.reply("%s (%s ago): %s", data.author.name, ago, data.message);
-    return irc.STATUS.STOP;
   });
+  return irc.STATUS.STOP;
 }
 
 function issue(msg, owner, project, num) {
@@ -22,8 +22,8 @@ function issue(msg, owner, project, num) {
   shared.getJSON(url, function(data) {
     let ago = shared.timeAgo(new Date(data.created_at)).slice(0, 2).join(" ");
     msg.reply("Issue #%d by %s (%s ago): %s", num, data.user.login, ago, data.title);
-    return irc.STATUS.STOP;
   });
+  return irc.STATUS.STOP;
 }
 
 function pullRequest(msg, owner, project, num) {
@@ -33,8 +33,8 @@ function pullRequest(msg, owner, project, num) {
     let ago = shared.timeAgo(new Date(data.created_at)).slice(0, 2).join(" ");
     log.debug("pull data", data);
     msg.reply("Pull request #%d by %s (%s ago): %s", num, data.user.login, ago, data.title);
-    return irc.STATUS.STOP;
   });
+  return irc.STATUS.STOP;
 }
 
 function load(bot) {
